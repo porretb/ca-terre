2a2ae6f4297541371e361eb6946b5ed75b897c00
bp <porretb@gmail.com>
2022-12-18 13:52:46 +0100

    modification terre08

--- terre08.js
+++ terre08.js
@@ -1,17 +1,27 @@
-let nb1 = process.argv[2];
-let nb2 = process.argv[3];
-let calc = 1;
-function puissance(nb1, nb2){
-	while (nb2 > 0){
-		calc = calc * nb1;
-		nb2--;
+let x = process.argv[2]; // nombre de base
+let y = process.argv[3]; // puissance
+let res = 1; // resultat
+function puissance(ag1, ag2){
+	while (y > 0){
+		res = res * x;
+		y--;
 	}
-	return (calc);
+	return (res);
 }
-if (nb2[0] == '-')
-{
-console.log('negatif');
+if (isNaN(x) && x != undefined || isNaN(y)){
+	console.log('pas de charactère merci');
+}
+else if (x[0] == '-' || y[0] == '-'){
+	console.log('pas de nombre negatif sorry');
+}
+else if (process.argv[4] != undefined){
+	console.log('Il y a trop d\'arguements');
+}
+else if (y == undefined || x == undefined){
+	console.log('il manque un arguement');
 }
 else
-console.log(puissance(nb1, nb2))
-
+{
+puissance(x,y);
+console.log(res);
+}
